16c3e4029e161d6f2a6a8d4f85991d77ebfaa8a0
sree296 <nivas296@gmail.com>
2018-08-11 15:04:45 +0530

    Disable submit btn and firstname validation

--- my-app/src/app/employees/create-employee.component.ts
+++ my-app/src/app/employees/create-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '../../../node_modules/@angular/forms';
+import {Department} from '../models/department.model';
+import {Employee} from '../models/employee.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,13 +10,38 @@ import { NgForm } from '../../../node_modules/@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  
+gender = "male";
+isActive = true;  
+department = "3";
+previewPhoto = false;
+
+employee: Employee = {
+  id: null,
+    name: null,
+    gender: null,
+    contactPreference: null,
+    phoneNumber: null,
+    email: null,
+    dateOfBirth: null,
+    department: null,
+    isActive: null,
+    photoPath: null
+};
+togglePhotoPreview(){
+  this.previewPhoto = !this.previewPhoto;
+}
+  departments : Department[] = [
+    {id:1, name:"Help Desk"},
+    {id:2, name:"HR"},
+    {id:3, name:"IT"},
+    {id:4, name:"Payroll"}
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
-  saveEmployee(empForm: NgForm) : void{
-    console.log(empForm.value);
+  saveEmployee(newEmployee: Employee) : void{
+    console.log(newEmployee);
   } 
 }
