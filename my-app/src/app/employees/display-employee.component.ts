5962a2fc14ebe77f93600abc5aaea5157c737c56
gattusr <sreenivasulu.gattu@cdk.com>
2018-10-27 13:20:48 +0530

    get and set

--- my-app/src/app/employees/display-employee.component.ts
+++ my-app/src/app/employees/display-employee.component.ts
@@ -6,12 +6,25 @@ import {Employee} from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit {
+export class DisplayEmployeeComponent implements OnInit{
+  
+  private _employee : Employee;
 
-  @Input() employee : Employee;
+  @Input()
+
+  set employee (val : Employee){
+    console.log('Previous: ' + (this._employee ? this._employee.name : 'NULL'));
+    console.log('Current: ' + val.name);
+    this._employee = val;
+  }
+
+  get employee(): Employee{
+    return this._employee;
+  }
+  
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
